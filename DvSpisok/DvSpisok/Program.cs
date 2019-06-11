d766a02534c3d9d61376a1b7513d57a8338b3fc9
Овчинникова Аделина <deley@mail.ru>
2019-06-12 00:41:56 +0500

    Add function ShowListTwo and Delete

--- DvSpisok/DvSpisok/Program.cs
+++ DvSpisok/DvSpisok/Program.cs
@@ -41,7 +41,7 @@ namespace DvSpisok
             string x = "";
             if (HowCreate == 1)
             {
-                Color.Print("\nДопускается ввод букв и знаков препинаний (!.?,;:)\n", ConsoleColor.Yellow);
+                Color.Print("\n Допускается ввод букв и знаков препинаний (!.?,;:)\n", ConsoleColor.Yellow);
                 Color.Print("\n Введено " + i + " из " + size + " элементов: ", ConsoleColor.Green);
                 x = Number.Symbols();
                 return new PointTwo(x);
@@ -51,6 +51,56 @@ namespace DvSpisok
             Color.Print("\n Добавлено!", ConsoleColor.Cyan);
             return new PointTwo(x);
         }
+
+        static void ShowListTwo()
+        {
+            Console.WriteLine("");
+            if (beg == null)
+            {
+                Color.Print("Список пуст!\n", ConsoleColor.Red);
+                return;
+            }
+            PointTwo temp = beg;
+            Color.Print("Исходный список:\n", ConsoleColor.Yellow);
+            while (temp != null)
+            {
+                Console.Write(temp);
+                temp = temp.next;//переход к следующему элементу
+            }
+        }
+
+        public static PointTwo Delete()
+        {
+            if (beg == null)//список пустой
+            {
+                Color.Print(" Список пуст!\n ", ConsoleColor.Red);
+                return null;
+            }
+            ShowListTwo();
+            Color.Print(" Номер удаляемого элемента: ", ConsoleColor.Green);
+            int number = Number.Check(1, int.MaxValue);
+            if (number == 1) //удаление в начале списка
+            {
+                beg = beg.next;
+                if (beg != null)
+                    beg.pred = null;
+                return beg;
+            }
+            PointTwo p = beg;
+            for (int i = 1; i < number && p != null; i++) p = p.next;
+            if (p == null)//если элемент не найден
+            {
+                Color.Print("Ошибка! Размер списка меньше, чем номер.\n", ConsoleColor.Red);
+                return beg;
+            }
+            //исключаем элемент из списка
+            p.pred.next = p.next;
+            if (p.next != null)
+                p.next.pred = p.pred;
+            Color.Print("\n Удалено!", ConsoleColor.Cyan);
+            return beg;
+        }
+
         static void Main()
         {
 
